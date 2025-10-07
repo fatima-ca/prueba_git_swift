31911cf2eb0c1ae647fe7b36152868e7bcd8665f
Fátima Castillo <fatimacastilloag@gmail.com>
2025-10-06 21:40:34 -0600

    Initial Commit

---
+++ pruebas_aplicacion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pruebas_aplicacion
+//
+//  Created by Fátima Castillo Aguirre on 06/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pruebas_aplicacion/pruebas_aplicacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebas_aplicacionApp.swift
+//  pruebas_aplicacion
+//
+//  Created by Fátima Castillo Aguirre on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct pruebas_aplicacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
